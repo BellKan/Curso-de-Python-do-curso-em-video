5345a82427f15e8415d4d34129699d8e31a3f467
StudioLontraGames <96369475+BellKan@users.noreply.github.com>
2024-04-01 17:47:53 -0300

    update e aula-3

---
+++ modulo-1/aula-3/index.py
@@ -0,0 +1,13 @@
+#Tipos primitivos
+
+# int [7, -4, 0, 9875]
+# float [4.5, 0.076, -15.223, 7.0]
+# bool [True, False]
+# str ['olá', '7.5', '']
+
+# forma diferente de print
+
+# o antigo -> print('A soma vale', s)
+
+#O novo -> print('A soma vale {}'.format(s))
+
