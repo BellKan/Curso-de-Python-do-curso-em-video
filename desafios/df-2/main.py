5df72b790095efa27002ef9a64bdc3b90b8954bd
StudioLontraGames <96369475+BellKan@users.noreply.github.com>
2024-04-01 12:59:38 -0300

    df-2
    comcluido!

---
+++ desafios/df-2/main.py
@@ -0,0 +1,5 @@
+dia = input ('Em que dia você nasceu?')
+mês = input ('Em que mês você nasceu?')
+ano = input ('Em que ano você nasceu?')
+
+print ('Você nasceu no dia', dia, 'do mês de', mês, 'do ano', ano)
